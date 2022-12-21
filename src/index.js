4f2a3fce2be729a4f0858b2a3c62dd78a43382ef
Layane Nunes <layane@MacBook-Air-de-Layane.local>
2022-12-21 17:01:50 -0300

    Requisito 01

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
-//arquivo app
-const express = require("express");
+const express = require('express');
 const talkerRouters = require('./routers/talkers.router');
 
 const app = express();
 app.use(talkerRouters);
 
 const HTTP_OK_STATUS = 200;
-const PORT = "3000";
+const PORT = '3000';
 
 // não remova esse endpoint, e para o avaliador funcionar
-app.get("/", (_request, response) => {
+app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
 app.listen(PORT, () => {
-  console.log("Online");
+  console.log('Online');
 });
